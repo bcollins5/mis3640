7f2503ac150fa848f23639785840d12ee7d1d168
bcollins5 <redsx1fan1@hotmail.com>
2016-09-19 23:39:58 -0400

    updates for session 5

--- mypolygon.py
+++ mypolygon.py
@@ -1,6 +1,6 @@
-import turtle
+'''#import turtle
 
-'''
+
 #Excercise 2.1
 def square(t):
     for i in range(4):     #we repeat same thing four times
@@ -10,7 +10,7 @@ def square(t):
 '''
 
 
-
+'''
 #Exercise 2.2
 
 def square(t, length):
@@ -21,7 +21,7 @@ def square(t, length):
 
 #Exercise 2.3
 def square(t, length):
-    for i in range(n):     #we repeat same thing four times
+    for i in range(n):     #we repeat same thing n times
         t.fd(length)
         t.lt(360/n)
 
@@ -48,7 +48,7 @@ turtle.mainloop()
 #Exercise 2.4
 
 
-'''
+
 def square(t, length):
     t.fd(length)
     t.lt(90)
@@ -60,9 +60,8 @@ def square(t, length):
 
 
 
-'''    
+    
 
-'''
 def polygon(t, length, n):
     #needed to be modified
     t.fd(length)
@@ -81,9 +80,147 @@ jerry = turtle.Turtle()
 
 square(jerry)
 
-#teresa = turtle.Turtle()
-#square(teresa)
+teresa = turtle.Turtle()
+square(teresa)
 
 
 turtle.mainloop()
 '''
+
+#  Yin & Yang with some color
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+from turtle import *
+
+def yin(radius, color1, color2):
+    width(3)
+    color("black", color1)
+    begin_fill()
+    circle(radius/2., 180)
+    circle(radius, 180)
+    left(180)
+    circle(-radius/2., 180)
+    end_fill()
+    left(90)
+    up()
+    forward(radius*0.35)
+    right(90)
+    down()
+    color(color1, color2)
+    begin_fill()
+    circle(radius*0.15)
+    end_fill()
+    left(90)
+    up()
+    backward(radius*0.35)
+    down()
+    left(90)
+
+def main():
+    reset()
+    yin(200, "black", "white")
+    yin(200, "white", "black")
+    ht()
+    return "Done!"
+
+if __name__ == '__main__':
+    main()
+    #mainloop()
+
+
+
+
+#  Practicing around with some other designs
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
+'The Number 3 ... working on the 0 part, for #30'
+'Trying to create a jersey image throughout the semester, maybe'
+
+
+import turtle
+jerry = turtle.Turtle()
+print(jerry)
+
+
+
+def square(t, length):
+    for i in range(4):     #we repeat same thing n times
+        t.fd(length)
+        t.lt(90)
+
+square(jerry, 100)
+
+jerry.fd(100)
+
+square(jerry, 100)
+
+jerry.fd(100)
+
+square(jerry, 100)
+
+jerry.fd(100)
+jerry.rt(90)
+jerry.fd(100)
+jerry.rt(90)
+jerry.fd(100)
+jerry.rt(90)
+jerry.fd(100)
+jerry.rt(90)
+jerry.fd(100)
+jerry.rt(90)
+jerry.fd(100)
+jerry.rt(90)
+
+square(jerry, 100)
+
+jerry.fd(100)
+
+square(jerry, 100)
+jerry.rt(180)
+jerry.fd(100)
+jerry.rt(90)
+jerry.fd(200)
+jerry.rt(90)
+jerry.fd(100)
+jerry.rt(90)
+jerry.fd(100)
+jerry.rt(90)
+jerry.fd(100)
+jerry.rt(90)
+jerry.fd(100)
+jerry.fd(100)
+jerry.rt(90)
+jerry.fd(300)
+jerry.rt(90)
+jerry.fd(100)
+jerry.rt(90)
+jerry.fd(100)
+jerry.rt(90)
+jerry.fd(100)
+jerry.rt(180)
+jerry.fd(100)
+jerry.rt(90)
+jerry.fd(100)
+jerry.rt(90)
+jerry.fd(100)
+jerry.rt(270)
+
+
+
+turtle.mainloop()
+
+
+'''def polygon(t, n, length):
+    angle = 360 / n
+    for i in range(n):
+        t.fd(length)
+        t.lt(angle)
+
+
+polygon(jerry, 7, 70)'''
+
+
